2316bff2ce73d0815bf307a0fbe1ac4b7cfbdf00
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2025-04-07 20:07:15 +0530

    ui: added UI for todo list

---
+++ src/components/Todo.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
+import "./Todo.css";
+import { Button, Paper, Typography } from "@mui/material";
+import CheckboxList from "./CheckboxList";
+
+interface Task {
+    isCompleted: boolean;
+    name: string;
+}
+
+const Todo: React.FC = () => {
+    const [list, setList] = useState<Task[]>([]);
+
+    return (
+        <div className="todo">
+            {/* header */}
+            <header>
+                <h2>My tasks</h2>
+                <span title="Block distracting sites while working.">
+                    <SettingsIcon className="icon" color="primary" />
+                </span>
+            </header>
+
+            {/* Add task */}
+            <Button
+                variant="outlined"
+                size="small"
+                startIcon={<AddCircleOutlineRoundedIcon />}
+                fullWidth
+                className="addTaskBtn"
+            >
+                Add task
+            </Button>
+
+            {/* display list */}
+            <CheckboxList />
+        </div>
+    );
+};
+
+export default Todo;
